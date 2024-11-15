22dc1752a5894134fb583f96f6fb30abb88e52dc
Seraphim De Bruyn <sdebruyn@sata02.univ-lr.fr>
2024-11-15 14:36:34 +0100

    feat : avancement de la page d'acceuil

--- sfapp/src/Form/AjoutSAType.php
+++ sfapp/src/Form/AjoutSAType.php
@@ -2,36 +2,37 @@
 
 namespace App\Form;
 
-use App\Entity\BatimentSalle;
-use App\Entity\EtageSalle;
 use App\Entity\SA;
-use App\Entity\Salle;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class AjoutSAType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // Champ Nom
             ->add('nom', TextType::class, [
                 'required' => true,
                 'attr' => [
-                    'placeholder' => 'Choisir un nom de SA..',
-                ]
+                    'placeholder' => 'Choisir un nom de SA..', // Placeholder pour le champ
+                ],
+                'label' => 'Nom du système d\'acquisition',
+                'label_attr' => [
+                    'class' => 'form-label'  // Classe pour le label
+                ],
+                'attr' => [
+                    'class' => 'form-control'  // Classe pour le champ
+                ],
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => SA::class,
-
+            'data_class' => SA::class,  // Associe ce formulaire à l'entité SA
         ]);
     }
 }
