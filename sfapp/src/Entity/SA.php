94c9173e98b8b820497d87efc5bd29f6257f901e
cpiovesa <cpiovesa@etudiant.univ-lr.fr>
2024-12-11 09:30:26 +0100

    Redisign sa liste

--- sfapp/src/Entity/SA.php
+++ sfapp/src/Entity/SA.php
@@ -53,16 +53,6 @@ class SA
         return $this;
     }
 
-    public function getDateAjout(): ?\DateTime
-    {
-        return $this->dateAjout;
-    }
-
-    public function setDateAjout(\DateTime $dateAjout): static
-    {
-        $this->dateAjout = $dateAjout;
-        return $this;
-    }
 
     public function getCapteurs(): Collection
     {
