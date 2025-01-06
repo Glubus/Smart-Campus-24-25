58d5bcfa21c1a9bc9968d26aa7c5bddb25fc9186
cpiovesa <cpiovesa@etudiant.univ-lr.fr>
2025-01-06 16:09:49 +0100

    FEAT : visualisation d'une liste de technicien

--- sfapp/src/Controller/AccueilController.php
+++ sfapp/src/Controller/AccueilController.php
@@ -11,7 +11,7 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_page_acceuil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig');
+        return $this->render('accueil/liste.html.twig');
     }
 }
 
